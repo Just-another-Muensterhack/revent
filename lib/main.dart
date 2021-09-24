de6b4010cc241f974f414d9cc0b1380a07cb8f82
DasProffi <67233923+DasProffi@users.noreply.github.com>
2021-09-24 23:39:36 +0200

    Issue 26 (#44)
    * added home page
    
    * added Routing to HomePage;
    updated HomePage Design;
    added Genres
    
    * updated main
    
    Co-authored-by: use-to <use-to@outlook.de>

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:revent/constants/colors.dart';
+import 'package:flutter/rendering.dart';
+import 'package:revent/pages/home_page.dart';
 import 'package:revent/pages/landing_page.dart';
 import 'package:revent/pages/splash_screen.dart';
 import 'package:revent/services/auth_service.dart';
 
 const String AppTitle = "revent";
+const Color primary = Color.fromRGBO(31, 38, 49, 1.0);
+const Color secondary = Color.fromRGBO(130, 81, 202, 1.0);
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -50,7 +53,7 @@ class _AppState extends State<App> {
 
       FirebaseAuth.instance.userChanges().listen((User user) {
         setState(() {
-          this._currentPage = user == null ? LandingPage() : Container();
+          this._currentPage = user == null ? LandingPage() : HomePage();
         });
       });
 

ab27c3f0aaefc73d9afab85593af8cece02ec45f
Jannik Wibker <jannikwibker@gmail.com>
2021-09-24 16:29:51 +0200

    added imageAppBar

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:revent/widget/custom_button.dart';
+import 'package:revent/widget/image_appbar.dart';
 
 class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: imageAppBar(),
       backgroundColor: Color(0xFF1F2631),
       body: Center(child: Column(
         children: <Widget>[
@@ -14,10 +16,10 @@ class LandingPage extends StatelessWidget {
             color: Colors.transparent,
             height: 40,
           ),
-          Center(child: Image(image: AssetImage('assets/images/logo_full.png'), width: 800/8, height: 340/8)),
+          // Center(child: Image(image: AssetImage('assets/images/logo_full.png'), width: 800/8, height: 340/8)),
           Container(
             color: Colors.transparent,
-            height: 260,
+            height: 230,
           ),
           Expanded(
             child: Column(

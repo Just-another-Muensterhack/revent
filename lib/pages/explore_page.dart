dfe7e4ceaa9e1cfb52779a604127b2a968011f8a
Felix Thape <felix.thape@gmail.com>
2021-09-24 23:18:51 +0200

    added Routing to HomePage; updated HomePage Design; added Genres

---
+++ lib/pages/explore_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ExplorePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Explores"),
+    );
+  }
+
+}

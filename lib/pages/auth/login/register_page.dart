b9bbfb53fd7e0d61662799c1943eb13c0a7232f1
3onier <30025379+3onier@users.noreply.github.com>
2021-09-24 14:53:41 +0200

    Added First Form for Registration

--- lib/pages/auth/login/register_page.dart
+++ lib/pages/auth/login/register_page.dart
@@ -1,19 +1,64 @@
-
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget{
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-
+  State<StatefulWidget> createState() => _RegisterPageState();
 }
 
 class _RegisterPageState extends State<RegisterPage>{
+
+  int _stepperIndex = 0;
+
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+
+  InputDecoration inpDec = new InputDecoration(
+    border: OutlineInputBorder(),
+  );
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO Generic App bar"),
+      ),
+      body: Stepper(
+        currentStep: _stepperIndex,
+        onStepContinue: () => this.setState(() {
+          _stepperIndex++;
+        }),
+        onStepCancel: () => this.setState(() {
+          _stepperIndex--;
+          if(_stepperIndex < 0){
+            Navigator.pop(context);
+          }
+        }),
+        onStepTapped: (int index) {
+          setState(() { _stepperIndex = index; });
+        },
+        steps: [
+          Step(
+              title: Text("Login with Google"),
+              content: Form(
+                child: Column(
+                  children: [
+                    TextFormField(),
+                  ],
+                ),
+              )
+          ),
+          Step(
+              title: Text("Your data"),
+              content: Form(
+                child: Column(
+                  children: [
+                    TextFormField(),
+                    TextFormField(),
+                  ],
+                ),
+              )
+          )
+        ],
+      ),
+    );
   }
 }

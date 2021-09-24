60069e5d6bc27f13a6596fc75aff21429bdd6aa1
Jannik Wibker <jannikwibker@gmail.com>
2021-09-24 16:02:03 +0200

    added landing page

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:revent/widget/custom_button.dart';
+
+class LandingPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1F2631),
+      body: Center(child: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.transparent,
+            height: 40,
+          ),
+          Center(child: Image(image: AssetImage('assets/images/logo_full.png'), width: 800/8, height: 340/8)),
+          Container(
+            color: Colors.transparent,
+            height: 260,
+          ),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                const Text("wanna celebrate your life?", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w600)),
+                Container(
+                  color: Colors.transparent,
+                  height: 40,
+                ),
+                Container(
+                  child: CustomButton(buttonText: "Get Started", color: Colors.white, onPress: () {}),
+                  height: 40,
+                )
+
+              ]
+            )
+          ),
+          Container(
+            color: Colors.transparent,
+            height: 260,
+          ),
+        ]
+      ))
+    );
+  }
+}

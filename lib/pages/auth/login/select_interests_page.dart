350083042fb40d6596b22bd37413e57a224a3331
Florian Sabonchi <sabonchi@posteo.de>
2021-09-24 19:53:07 +0200

    button for continue added

--- lib/pages/auth/login/select_interests_page.dart
+++ lib/pages/auth/login/select_interests_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:revent/constants/colors.dart';
 import 'package:revent/models/commons.dart';
+import 'package:revent/widget/custom_button.dart';
+import 'package:revent/widget/image_appbar.dart';
 
 class SelectInterestsPage extends StatefulWidget{
   @override
@@ -24,29 +27,35 @@ class _SelectInterestsPageState extends State<SelectInterestsPage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(height: 200,
-                child:  ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: Genre.values.length,
-                  itemBuilder: (BuildContext context, int index) => Card(color: _cardsValue[index] ?
-                    Colors.lime:  Colors.grey,
-                    child: InkWell(onTap: (){
-                        setState(() {
-                          _cardsValue[index] = !_cardsValue[index];
-                        });
-                      },
-                      child: Container(width: size.width * 0.25, child: Center(child: Text(
-                      Genre.values[index].toString().split(".").last,
-                        style: TextStyle(color: Colors.white),)
-                    )),)
-                  ),), ),
-          ],
-        ))
+    return Scaffold(appBar: imageAppBar(),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height: 200,
+                  child:  ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: Genre.values.length,
+                    itemBuilder: (BuildContext context, int index) => Card(color: _cardsValue[index] ?
+                      primary:  Colors.grey,
+                      child: InkWell(onTap: (){
+                          setState(() {
+                            _cardsValue[index] = !_cardsValue[index];
+                          });
+                        },
+                        child: Container(width: size.width * 0.25, child: Center(child: Text(
+                        Genre.values[index].toString().split(".").last,
+                          style: TextStyle(color: Colors.white),)
+                      )),)
+                    ),), ),
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: CustomButton(buttonText: "Continue", onPress: () {
+
+                },),)
+            ],
+          ))
     );
   }
 }

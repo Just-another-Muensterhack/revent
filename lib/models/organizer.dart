f7b5a46a30c1ae879b6593f75f5855a2731acdd9
Felix Thape <felix.thape@gmail.com>
2021-09-24 16:49:11 +0200

    added and updated organizer

--- lib/models/organizer.dart
+++ lib/models/organizer.dart
@@ -1,36 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:revent/models/commons.dart';
-
 
 class Organizer {
   String databaseID;
   String name;
   String address;
   String websiteURL;
-  List<String> owners;	
+  List<String> owners;
 
-  static final _orgranizerRef =
-      FirebaseFirestore.instance.collection('organizer').withConverter<Organizer>(
-            fromFirestore: (snapshot, _) => Organizer._fromJson(snapshot.data()),
-            toFirestore: (organizer, _) => orgranizer._toJson(),
-          );
+  static final _databaseRef = FirebaseFirestore.instance
+      .collection('organizer')
+      .withConverter<Organizer>(
+        fromFirestore: (snapshot, _) => Organizer._fromJson(snapshot.data()),
+        toFirestore: (organizer, _) => organizer._toJson(),
+      );
 
   Organizer._(this.name, this.address, owner, {this.websiteURL = ""}) {
     this.owners.add(owner);
   }
 
-  Profile._fromJson(Map<String, Object> json) {
+  Organizer._fromJson(Map<String, Object> json) {
     this.databaseID = json['id'] as String;
     this.name = json['name'] as String;
     this.address = json['address'] as String;
     this.websiteURL = json['websiteURL'] as String;
-    this.favorites = json['owners'] as List<String>;
+    this.owners = json['owners'] as List<String>;
   }
 
   Map<String, Object> _toJson() {
     return {
-      'id': this.databaseID,
       'name': this.name,
       'address': this.address,
       'websiteURL': this.websiteURL,
@@ -39,25 +36,36 @@ class Organizer {
   }
 
   Future<void> delete() async {
-    return await _profileRef
+    return await _databaseRef
         .doc(this.databaseID)
         .delete()
         .then((_) => print("Organizer Deleted"))
         .catchError((error) => print("Failed to delete Organizer: $error"));
   }
 
-  static Future<Profile> create(name, address, owner) async {
-    Profile event = Profile._(name, address, owner);
-    event.save();
+  static Future<Organizer> create(name, address, owner) async {
+    Organizer organizer = Organizer._(name, address, owner);
+    organizer.save();
 
-    return event;
+    return organizer;
   }
 
   Future<void> save() async {
     if (this.databaseID == null || this.databaseID.isEmpty) {
-      await _orgranizerRef.add(this).then((value) => this.databaseID = value.id);
+      await _databaseRef.add(this).then((value) => this.databaseID = value.id);
     } else {
-      await _orgranizerRef.doc(this.databaseID).update(this._toJson());
+      await _databaseRef.doc(this.databaseID).update(this._toJson());
     }
+  }
+
+  static Future<Organizer> getOrganizer(String organizerRef) async {
+    Organizer organizer;
+    await _databaseRef
+        .where('id', isEqualTo: organizerRef)
+        .limit(1)
+        .get()
+        .then((value) => organizer = value.docs.first.data());
+
+    return organizer;
   }
 }

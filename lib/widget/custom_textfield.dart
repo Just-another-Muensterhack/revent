6801cdb61fe528c7f0e170b279811f62fb725e5e
3onier <30025379+3onier@users.noreply.github.com>
2021-09-24 15:24:39 +0200

    Added Custom Textfield

---
+++ lib/widget/custom_textfield.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomInput extends StatelessWidget{
+  String hintText = '';
+  final ValueChanged<String> onChanged;
+  final bool obscureText;
+
+  CustomInput({Key? key,
+    required this.hintText,
+    required this.onChanged,
+    this.obscureText = false}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color:Colors.white,
+        border: Border.all(),),
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      child: TextField(onChanged: onChanged,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            hintText: hintText,
+            border: InputBorder.none
+        ),
+      ),
+    );
+  }
+}

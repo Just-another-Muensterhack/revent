a24a974efd6453dfed0436883c5890ddd5aef372
Florian Sabonchi <sabonchi@posteo.de>
2021-09-25 01:37:27 +0200

    fixed formating

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -15,94 +15,68 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
         body: Container(
             child: Column(children: <Widget>[
-      Padding(
-        padding: EdgeInsets.only(top: 50, left: 50, bottom: 10),
-        child: Row(
-          children: <Widget>[],
-        ),
-      ),
-      Container(
-          width: size.width * 0.8,
-          decoration: BoxDecoration(
+              Padding(
+                padding: EdgeInsets.only(top: 50, left: 50, bottom: 10),
+                child: Row(children: <Widget>[],),),
+              Container(width: size.width * 0.8, decoration: BoxDecoration(
               color: Colors.deepPurpleAccent,
               borderRadius: BorderRadius.circular(25)),
-          child: Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(15),
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage(
-                      "https://www.appmanufacture.ch/wp-content/uploads/2016/10/2_small.png"),
-                ),
-              ),
-              Column(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(top: 15),
-                    child: Text(
-                      "Max",
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(bottom: 15),
-                    child: Text(
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(15), child: CircleAvatar(radius: 50,
+                        backgroundImage: NetworkImage(
+                      "https://www.appmanufacture.ch/wp-content/uploads/2016/10/2_small.png"),),),
+                      Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 15),
+                            child: Text("Max", style: TextStyle(fontSize: 18, color: Colors.white),),),
+                          Padding(padding: EdgeInsets.only(bottom: 15), child: Text(
                       "Mustermann",
                       style: TextStyle(fontSize: 18, color: Colors.white),
                     ),
-                  ),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 15,
-                ),
-                child: IconButton(onPressed: () {}, icon: Icon(Icons.qr_code)),
+                  ),],),
+                      Padding(padding: EdgeInsets.only(
+                        left: 15,),
+                        child: IconButton(onPressed: () {}, icon: Icon(Icons.qr_code)),
               )
-            ],
-          )),
-      Container(height: 40),
-      Container(
-          width: size.width * 0.8,
-          decoration: BoxDecoration(
+            ],)), Container(height: 40), Container(width: size.width * 0.8, decoration: BoxDecoration(
               color: Colors.deepPurpleAccent,
-              borderRadius: BorderRadius.circular(25)),
-          child: Column(children: <Widget>[
-            ListTile(
-                title: const Text("Account",
+              borderRadius: BorderRadius.circular(25)), child: Column(children: <Widget>[
+                ListTile(
+                    title: const Text("Account",
                     style: TextStyle(color: Colors.white)),
-                trailing: IconButton(
-                    icon: const Icon(Icons.arrow_right),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.arrow_right),
                     color: Colors.white,
                     onPressed: () => {})),
-            ListTile(
-                title: const Text("Friends",
+                ListTile(
+                    title: const Text("Friends",
                     style: TextStyle(color: Colors.white)),
-                trailing: IconButton(
-                    icon: const Icon(Icons.arrow_right),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.arrow_right),
                     color: Colors.white,
                     onPressed: () => {})),
-            ListTile(
-                title: const Text("Notifications",
+                ListTile(
+                    title: const Text("Notifications",
                     style: TextStyle(color: Colors.white)),
-                trailing: IconButton(
-                    icon: const Icon(Icons.arrow_right),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.arrow_right),
                     color: Colors.white,
                     onPressed: () => {})),
-            // divider here pls :3
-            ListTile(
-                title: const Text("Data & Privacy",
+                ListTile(
+                    title: const Text("Data & Privacy",
                     style: TextStyle(color: Colors.white)),
-                trailing: IconButton(
-                    icon: const Icon(Icons.arrow_right),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.arrow_right),
                     color: Colors.white,
                     onPressed: () => {})),
-            ListTile(
-                title: const Text("Impressum",
+                ListTile(
+                    title: const Text("Impressum",
                     style: TextStyle(color: Colors.white)),
-                trailing: IconButton(
-                    icon: const Icon(Icons.arrow_right),
+                    trailing: IconButton(
+                        icon: const Icon(Icons.arrow_right),
                     color: Colors.white,
                     onPressed: () => {}))
           ]))

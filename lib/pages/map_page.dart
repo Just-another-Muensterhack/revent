e64e87acd58813a04f66803f1d7c9378bba9559a
3onier <30025379+3onier@users.noreply.github.com>
2021-09-25 11:22:51 +0200

    Cleaned CI

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,8 +1,6 @@
 
 import 'package:flutter/cupertino.dart';
-import 'package:revent/models/commons.dart';
 import 'package:revent/models/event.dart';
-import 'package:revent/models/location.dart';
 import 'package:revent/widgets/event_map_widget.dart';
 
 class MapPage extends StatelessWidget {

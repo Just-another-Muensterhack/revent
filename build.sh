fdc8d1b4ca69b7d3a3e5eb15d99bf3b1c0280a15
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2021-09-25 14:59:51 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 # build script for cloudflare pages
 
 curl -qo- https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_2.5.1-stable.tar.xz | tar xJ
+./flutter/bin/flutter upgrade
 ./flutter/bin/flutter pub get
 ./flutter/bin/flutter build web
